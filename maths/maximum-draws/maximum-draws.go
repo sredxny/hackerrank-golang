9c983a3bba36a8e9ff89c6faae6ebd52f59cafb2
sredny <sredny.buitrago@gmail.com>
2019-07-23 22:24:52 -0500

    added maximum drwas exercises

---
+++ maths/maximum-draws/maximum-draws.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the maximumDraws function below.
+ */
+func maximumDraws(n int32) int32 {
+
+     return n+1
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 1024 * 1024)
+
+    tTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+    checkError(err)
+    t := int32(tTemp)
+
+    for tItr := 0; tItr < int(t); tItr++ {
+        nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+        checkError(err)
+        n := int32(nTemp)
+
+        result := maximumDraws(n)
+
+        fmt.Fprintf(writer, "%d\n", result)
+    }
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
